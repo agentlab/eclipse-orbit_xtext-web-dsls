49ab2ed4e9b8f27faa02fa4afcb758dc5542879c
Dmitry Zagrebaev <you@example.com>
2016-10-07 22:01:01 +0300

    Web прилетает через бинарные зависимости

--- org.eclipse.xtext.web.dsl.launguage/src/org/eclipse/xtext/web/dsl/launguage/IExecutorServiceProvider.xtend
+++
@@ -1,8 +0,0 @@
-package org.eclipse.xtext.web.dsl.launguage
-
-import com.google.inject.Provider
-import java.util.concurrent.ExecutorService
-
-public interface IExecutorServiceProvider {
-	public def Provider<ExecutorService> getExecutorServiceProvider();
-}
--- org.eclipse.xtext.web.dsl.launguage/xtend-gen/org/eclipse/xtext/web/dsl/launguage/IExecutorServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.xtext.web.dsl.launguage;
-
-import com.google.inject.Provider;
-import java.util.concurrent.ExecutorService;
-
-@SuppressWarnings("all")
-public interface IExecutorServiceProvider {
-  public abstract Provider<ExecutorService> getExecutorServiceProvider();
-}
